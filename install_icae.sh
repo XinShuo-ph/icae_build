dfbf0ebafa237e177d34ea04dc9233a29d4d94ac
XinShuo-ph <13515544769@163.com>
2024-08-24 20:26:48 +0000

    more dependancies

--- install_icae.sh
+++ install_icae.sh
@@ -5,10 +5,8 @@ cd icae/code/icae_v1/peft
 pip install transformers==4.36.2
 # install peft dependancy
 pip install .
-# install flash_attn
-pip install flash_attn
-# Install SentencePiece
-pip install sentencepiece
+# install icaev_2 depandancies flash_attn etc. (found by trial and error...)
+pip install flash_attn protobuf sentencepiece
 # download and run v2
 cd ~/icae/code/icae_v2
 wget "https://huggingface.co/sggetao/icae/resolve/main/mistral_7b_ft_icae.safetensors"
